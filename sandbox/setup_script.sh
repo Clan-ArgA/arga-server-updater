016c9072f8ad90ab3f7b4e6fb6144917b2a2d65e
VulturARG <luisbriones@gmail.com>
2023-09-11 09:22:52 -0300

    Add setup_script.sh to .gitignore.

--- sandbox/setup_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/steam/scripts/
-git clone https://vulturARG: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@github.com/Clan-ArgA/arga-server-updater.git
-cd arga-server-updater
-git checkout stage-1_refactor_script
-echo "STEAM_USER=argasteam" > ./server_updater/.env
-echo "STEAM_PASS=wsx147zaqsteam" >> ./server_updater/.env
-pip install -r ./server_updater/requirements.txt
