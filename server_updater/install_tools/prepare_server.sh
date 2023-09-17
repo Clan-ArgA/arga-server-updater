d4c3a402262e57e930ae38c92c65d7538b3cad77
VulturARG <luisbriones@gmail.com>
2023-09-17 11:29:51 -0300

    Completed.

--- server_updater/install_tools/prepare_server.sh
+++ server_updater/install_tools/prepare_server.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-apt-get update && \
-    apt-get install -y --no-install-recommends --no-install-suggests \
-        git \
-        curl \
-        python3 \
-        nano \
-        pip \
-        rename \
-        lib32stdc++6 \
-        lib32gcc-s1 \
-        libcurl4 \
-        wget \
-        ca-certificates && \
-    apt-get remove --purge -y && \
-    apt-get clean autoclean &&  \
-    apt-get autoremove -y && \
-    rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get install -y --no-install-recommends --no-install-suggests \
+    curl \
+    python3 \
+    nano \
+    pip \
+    rename \
+    lib32stdc++6 \
+    lib32gcc-s1 \
+    libcurl4 \
+    wget \
+    ca-certificates
+apt-get remove --purge -y
+apt-get clean autoclean
+apt-get autoremove -y
+rm -rf /var/lib/apt/lists/*
+
+pip install --no-cache-dir -r requirements.txt
+
+steam_home="/home/steam"
+steamcmd_dir="$steam_home/steamcmd"
+
+if [ ! -d "$steam_home" ]; then
+    mkdir -p "$steam_home"
+fi
+if [ ! -d "$steamcmd_dir" ]; then
+    mkdir -p "$steamcmd_dir"
+fi
+cd /home/steam/steamcmd
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+
+cd /home/steam/scripts/arga-server-updater
+echo "STEAM_USER=anonymous" > ./server_updater/.env
+echo "STEAM_PASS=" >> ./server_updater/.env
