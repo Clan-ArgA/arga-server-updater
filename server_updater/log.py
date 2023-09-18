38adc8ddc3ab503d6e6cd57c6dd99ec72332f1f5
VulturARG <luisbriones@gmail.com>
2023-09-18 09:58:05 -0300

    Add flush.

--- server_updater/log.py
+++ server_updater/log.py
@@ -2,4 +2,4 @@ class Log:
     @staticmethod
     def log(msg: str) -> None:
         log_output = f"\n{'':=<{len(msg)}}\n{msg}\n{'':=<{len(msg)}}"
-        print(log_output)
+        print(log_output, flush=True)
