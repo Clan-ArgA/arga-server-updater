4417a605a30de7e03702585b3898900c875ae31b
VulturARG <luisbriones@gmail.com>
2023-09-23 10:19:44 -0300

    Check if folder exist.

--- server_updater/install_tools/copy_reforger_config.sh
+++ server_updater/install_tools/copy_reforger_config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mkdir /home/steam/steamcmd/reforger/config
+steamcmd_config="/home/steam/steamcmd/reforger/config"
+
+if [ ! -d "$steamcmd_config" ]; then
+    mkdir -p "$steamcmd_config"
+fi
+
 cp /home/steam/scripts/arga-server-updater/server_updater/reforger_config/*.json /home/steam/steamcmd/reforger/config
