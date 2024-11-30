0e024f105e691239dc4f567ecbaa75285d0a8943
Ignacio Kasevich <33383172+IgnacioKase@users.noreply.github.com>
2024-11-30 16:16:10 +0100

    Update server user

--- server_updater/config.py
+++ server_updater/config.py
@@ -5,7 +5,7 @@ import re
 
 load_dotenv()
 
-STEAM_CMD = "/home/steam/steamcmd/steamcmd.sh"
+STEAM_CMD = "/home/arma/steamcmd/steamcmd.sh"
 STEAM_USER = os.getenv("STEAM_USER")
 STEAM_PASS = os.getenv("STEAM_PASS")
 
@@ -13,7 +13,7 @@ WORKSHOP_CHANGELOG_URL = "https://steamcommunity.com/sharedfiles/filedetails/cha
 PATTERN = re.compile(r"workshopAnnouncement.*?<p id=\"(\d+)\">", re.DOTALL)
 
 A3_SERVER_ID = "233780"
-A3_SERVER_DIR = "/home/steam/steamcmd/arma3"
+A3_SERVER_DIR = "/home/arma/steamcmd/arma3"
 A3_WORKSHOP_ID = "107410"
 A3_WORKSHOP_DIR = f"{A3_SERVER_DIR}/steamapps/workshop/content/{A3_WORKSHOP_ID}"
 A3_MODS_DIR = A3_SERVER_DIR
@@ -22,12 +22,12 @@ A3_MOD_KEYS_SOURCE_DIRECTORY = (
 )
 A3_MOD_KEYS_DESTINATION_DIRECTORY = f"{A3_SERVER_DIR}/keys"
 A3_MODS_LIST_PATH = (
-    "/home/steam/scripts/arga-server-updater/server_updater/arma3_mods_list"
+    "/home/arma/scripts/arga-server-updater/server_updater/arma3_mods_list"
 )
 A3_MOD_DEFAULT = "arga"
 
 REFORGER_SERVER_ID = "1874900"
-REFORGER_SERVER_DIR = "/home/steam/steamcmd/reforger"
+REFORGER_SERVER_DIR = "/home/arma/steamcmd/reforger"
 REFORGER_WORKSHOP_ID = "1874880"
 REFORGER_WORKSHOP_DIR = (
     f"{REFORGER_SERVER_DIR}/steamapps/workshop/content/{REFORGER_WORKSHOP_ID}"
